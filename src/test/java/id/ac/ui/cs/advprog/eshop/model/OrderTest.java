cb591bdee0e3bff73593418bb5124c4c6cfc02f3
Nadya Hoesin <nadya.hoesin@ui.ac.id>
2024-02-29 09:05:00 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -33,7 +33,7 @@ public class OrderTest {
         assertThrows(IllegalArgumentException.class, () -> {
             Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                     this.products, 1708560000L, "Safira Sudrajat");
-        })
+        });
     }
 
     @Test
@@ -64,7 +64,7 @@ public class OrderTest {
         assertThrows(IllegalArgumentException.class, () -> {
             Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                     this.products, 1708560000L, "Safira Sudrajat", "MEOW");
-        })
+        });
     }
 
     @Test
